b26ed941416b7f59903ad8a76bab752495466643
julielim2424 <julielim2424@gmail.com>
2026-06-10 23:08:55 +0900

    Update watch.py

--- watch.py
+++ watch.py
@@ -2,37 +2,21 @@ from playwright.sync_api import sync_playwright
 
 url = "https://www.tmdn.org/tmview/#/tmview/results?page=1&pageSize=30&criteria=C&basicSearch=LG&sortColumn=applicationDate&desc=true"
 
-def handle_response(response):
-    if "search/results" in response.url:
+def handle_request(request):
+    if "search/results" in request.url:
+        print("========== REQUEST ==========")
+
         try:
-            data = response.json()
-
-            print("===== DATA KEYS =====")
-            print(data.keys())
-
-            print("===== RESULT COUNT =====")
-            print(len(data.get("tradeMarks", [])))
-
-            print("===== FIRST 5 RESULTS =====")
-
-            for tm in data.get("tradeMarks", [])[:5]:
-                print(
-                    tm.get("tmName"),
-                    "|",
-                    tm.get("applicationNumber"),
-                    "|",
-                    tm.get("applicationDate")
-                )
-
-        except Exception as e:
-            print("ERROR:", e)
+            print(request.post_data)
+        except:
+            pass
 
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)
 
     page = browser.new_page()
 
-    page.on("response", handle_response)
+    page.on("request", handle_request)
 
     page.goto(url)
 
