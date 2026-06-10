3dfac3b43bf3ec9d05577141c7442ac30efe4f83
julielim2424 <julielim2424@gmail.com>
2026-06-10 22:18:22 +0900

    Update watch.py

--- watch.py
+++ watch.py
@@ -1,8 +1,1 @@
-import requests
-
-url = "https://www.tmdn.org/tmview/#/tmview/results?page=1&pageSize=30&criteria=C&basicSearch=LG&sortColumn=applicationDate&desc=true"
-
-r = requests.get(url)
-
-print("status:", r.status_code)
-print(r.text[:1000])
+print("JULIELIM TEST 999999")
