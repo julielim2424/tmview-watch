1c7199e345a6c571a001d9a7589f48dd1548afaa
julielim2424 <julielim2424@gmail.com>
2026-06-10 22:50:18 +0900

    Update watch.py

--- watch.py
+++ watch.py
@@ -2,17 +2,19 @@ from playwright.sync_api import sync_playwright
 
 url = "https://www.tmdn.org/tmview/#/tmview/results?page=1&pageSize=30&criteria=C&basicSearch=LG&sortColumn=applicationDate&desc=true"
 
+def log_response(response):
+    if "/api/" in response.url:
+        print("API:", response.url)
+
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)
 
     page = browser.new_page()
 
+    page.on("response", log_response)
+
     page.goto(url)
 
-    page.wait_for_timeout(8000)
-
-    print("TITLE:", page.title())
-
-    print(page.content()[:5000])
+    page.wait_for_timeout(10000)
 
     browser.close()
