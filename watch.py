919ba43843c430fa22923ea1c25036ac7584fa60
julielim2424 <julielim2424@gmail.com>
2026-06-10 22:38:26 +0900

    Update watch.py

--- watch.py
+++ watch.py
@@ -1,1 +1,20 @@
-print("JULIELIM TEST 999999")
+from playwright.sync_api import sync_playwright
+
+url = "https://www.tmdn.org/tmview/#/tmview/results?page=1&pageSize=30&criteria=C&basicSearch=LG&sortColumn=applicationDate&desc=true"
+
+print("START")
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=True)
+
+    page = browser.new_page()
+
+    page.goto(url)
+
+    page.wait_for_timeout(5000)
+
+    print("TITLE:", page.title())
+
+    browser.close()
+
+print("END")
