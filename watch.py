c47285bc7eea9c9c635ecc54217d2790499ad508
julielim2424 <julielim2424@gmail.com>
2026-06-10 22:07:06 +0900

    Update watch.py

--- watch.py
+++ watch.py
@@ -1,1 +1,8 @@
-print("TMVIEW WATCH STARTED")
+import requests
+
+url = "https://www.tmdn.org/tmview/#/tmview/results?page=1&pageSize=30&criteria=C&basicSearch=LG&sortColumn=applicationDate&desc=true"
+
+r = requests.get(url)
+
+print("status:", r.status_code)
+print(r.text[:1000])
